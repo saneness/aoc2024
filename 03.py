2b58269a099a85109f02024515a4482840641586
saneness <saneness@gmail.com>
2024-12-03 11:26:39 +0000

    day 3

---
+++ 03.py
@@ -0,0 +1,21 @@
+from aocd.models import Puzzle
+import re
+
+puzzle = Puzzle(year=2024, day=3)
+data   = puzzle.input_data
+
+rx = re.compile("mul\((\d+),(\d+)\)|(don't\(\)|do\(\))", re.VERBOSE)
+
+answer_a = 0
+answer_b = 0
+
+do = True
+for item in re.findall(rx, data):
+    if item[2] != "":
+        do = item[2] == "do()"
+    else:
+        answer_a += int(item[0])*int(item[1])
+        answer_b += int(item[0])*int(item[1]) if do else 0
+
+puzzle.answer_a = answer_a
+puzzle.answer_b = answer_b
